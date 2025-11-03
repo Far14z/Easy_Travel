ac5f1adb4c844d8428a3c364a2236262a8b62497
Far14z <faridce14@gmail.com>
2025-11-02 22:52:41 -0500

    feat: add bloc for auth service.

--- lib/features/auth/presentation/blocs/auth_bloc.dart
+++ lib/features/auth/presentation/blocs/auth_bloc.dart
@@ -5,16 +5,18 @@ import 'package:easy_travel/features/auth/presentation/blocs/auth_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc(): super(AuthInitialState()) {
+  AuthBloc() : super(AuthInitialState()) {
     on<LoginEvent>((event, emit) async {
       emit(AuthLoadingState());
       try {
-        final User user = await AuhtService.login(
+        final User user = await AuhtService().login(
           event.email,
-          event.password
-        )
-        
+          event.password,
+        );
+        emit(AuthSuccessState(user: user));
+      } catch (e) {
+        emit(AuthFailureState(message: e.toString()));
       }
-    },);
+    });
   }
 }
